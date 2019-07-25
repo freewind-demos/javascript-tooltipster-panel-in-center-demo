977927a906241d5dcdffb74fe969073cd564474d
Freewind <nowindlee@gmail.com>
2019-07-25 17:55:29 +0800

    demo

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-JavaScript Tooltipster Demo
-===========================
+JavaScript Tooltipster Panel in Center Demo
+============================================
+
+利用`functionPosition`计算新的position，可以自由定位。
 
 ```
 npm install
 npm run demo
 ```
 
+https://iamceege.github.io/tooltipster/#positioning
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(function () {
-  $('a').tooltipster();
+  $('a').tooltipster({
+    content: '<div id="tooltip-panel">Hello</div>',
+    contentAsHTML: true,
+    arrow: false,
+    functionPosition: function(instance, helper, position){
+      console.log('### position', position);
+      console.log('### window', $(window).width());
+
+      position.coord.left = ($(window).width() - position.size.width)/2;
+      return position;
+    }
+  });
 });
